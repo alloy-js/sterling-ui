417478afca4f7fb9acd54224614d5548836b0449
Tristan Dyer <atdyer@ncsu.edu>
2019-11-21 11:35:46 -0500

    Ace

--- src/components/views/custom-view/CustomViewStage.tsx
+++ src/components/views/custom-view/CustomViewStage.tsx
@@ -1,16 +1,21 @@
 import { AlloyInstance } from 'alloy-ts';
-import React from 'react';
+import React, { Ref } from 'react';
 import SplitPane from 'react-split-pane';
-import Editor from 'react-simple-code-editor';
-import hljs from 'highlight.js';
+// import Editor from 'react-simple-code-editor';
+// import hljs from 'highlight.js';
 // @ts-ignore
-import javascript from 'highlight.js/lib/languages/javascript';
-import 'highlight.js/styles/ocean.css';
+// import javascript from 'highlight.js/lib/languages/javascript';
+// import 'highlight.js/styles/ocean.css';
 import * as d3 from 'd3';
+import AceEditor from 'react-ace';
 
-hljs.registerLanguage('javascript', javascript);
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-github';
 
-const placeholder = 'Custom visualizations are created by running the code in this editor.\n\nWrite some code and press Ctrl+Enter to execute.';
+// hljs.registerLanguage('javascript', javascript);
+
+// const placeholder = 'Custom visualizations are created by running the code in this editor.\n\nWrite some code and press Ctrl+Enter to execute.';
 
 export interface ICustomViewStageProps {
     instance: AlloyInstance | null;
@@ -42,26 +47,24 @@ class CustomViewStage extends React.Component<ICustomViewStageProps> {
             <SplitPane
                 defaultSize='400px'
                 split='vertical'
-                style={{ position: 'relative' }}>
-                <Editor
-                    className='script-editor'
+                style={{ display: 'flex', position: 'relative' }}>
+                <AceEditor
+                    mode='javascript'
+                    theme='github'
+                    fontSize={14}
+                    enableBasicAutocompletion={true}
+                    enableLiveAutocompletion={true}
+                    showPrintMargin={false}
+                    wrapEnabled={true}
+                    width='100%'
+                    height='100%'
+                    onChange={this._on_change.bind(this)}
                     value={this.state.code}
-                    onValueChange={code => this.setState({code})}
-                    highlight={code => hljs.highlight('javascript', code).value}
-                    padding={10}
-                    placeholder={placeholder}
-                    onKeyDown={(e: React.KeyboardEvent) => {
-                        if (e.ctrlKey && e.key === 'Enter') {
-                            e.preventDefault();
-                            this._on_run();
-                        }
-                    }}
-                    style={{
-                        height: '100%',
-                        fontFamily: '"Fira Code", "Fira Mono", monospace',
-                        fontSize: '12px',
-                        overflowY: 'auto'
-                    }}/>
+                    commands={[{
+                        name: 'execute',
+                        bindKey: {win: 'Ctrl+Enter', mac: 'Command+Enter'},
+                        exec: () => this._on_run()
+                    }]}/>
                 <div style={{
                     width: '100%',
                     height: '100%',
@@ -126,7 +129,15 @@ class CustomViewStage extends React.Component<ICustomViewStageProps> {
 
     }
 
+    private _on_change (code: string) {
+
+        this.setState({ code: code });
+
+    }
+
     private _on_run () {
+
+        console.log('running');
 
         if (this.props.instance) {
 
